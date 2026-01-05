75bae5cef0ef84fdd762b5e5853970a73b7752b8
Rachel <rachellima>
2026-01-05 16:20:13 -0300

    image

--- app/src/main/java/com/rgb/samplejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/rgb/samplejetpackcompose/MainActivity.kt
@@ -4,20 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -37,22 +38,18 @@ fun Home() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.DarkGray),
+            .background(Color.White),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Canvas(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(350.dp)
-                .background(Color.White)
-        ) {
-            drawRect(
-                color = Color.Red,
-                size = Size(255f, 255f),
-                topLeft = Offset(x = 80f, y = 50f)
-            )
-        }
+        Image(
+            painter = painterResource(R.drawable.dog),
+            modifier = Modifier.size(300.dp).clip(
+                shape = RoundedCornerShape(20.dp)
+            ),
+            contentScale = ContentScale.FillBounds,
+            contentDescription = "dog"
+        )
     }
 
 }
